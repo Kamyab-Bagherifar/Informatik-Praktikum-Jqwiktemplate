a7e262195a65ce96f50ca41182bf9a8dd6cb465a
Uwe Krohn <krohn@fh-luebeck.de>
2021-02-25 14:35:57 +0100

    collatz comment

--- src/main/java/recursion/Methods.java
+++ src/main/java/recursion/Methods.java
@@ -7,7 +7,8 @@ public class Methods {
             : add(++x,--y);
   }
 
-  static int collatz(int n, int bla) {
+  static int collatz(int n) {
+    // comment
     if (n == 1) return 1;
     else if (n % 2 == 0) return collatz(n / 2);
     else return collatz(3*n + 1);
