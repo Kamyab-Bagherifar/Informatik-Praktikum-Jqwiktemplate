192b9b8f11f8a4eb3fdd92a3ca04c2fc38698623
Uwe Krohn <krohn@fh-luebeck.de>
2021-02-25 15:01:14 +0100

    collatz comment deleted again

--- src/main/java/recursion/Methods.java
+++ src/main/java/recursion/Methods.java
@@ -8,7 +8,6 @@ public class Methods {
   }
 
   static int collatz(int n) {
-    // comment
     if (n == 1) return 1;
     else if (n % 2 == 0) return collatz(n / 2);
     else return collatz(3*n + 1);
